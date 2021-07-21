1e8569955ba752f1c81c2764cd66a56a7e8c1fc6
DarkcontrolCC <87600728+DarkcontrolCC@users.noreply.github.com>
2021-07-21 16:31:21 +0700

    Add files via upload

---
+++ Exercise4_Natchanop_y.py
@@ -0,0 +1,11 @@
+FD_eng = 90.6
+GN_BU= 78.7
+int_comp_sym = 86.5
+Comp_Program = 92.7
+
+print("--------Your Score--------")
+print("Foundation English :",FD_eng,"Score")
+print("General Business :",GN_BU,"Score")
+print("Introduction to Computer System :",int_comp_sym,"Score")
+print("Computer Programming :",Comp_Program,"Score")
+print("--------------------------")
