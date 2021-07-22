fbf29ab93743124f098def02d7f92f2dac7aef66
DarkcontrolCC <87600728+DarkcontrolCC@users.noreply.github.com>
2021-07-22 16:25:26 +0700

    Exercise5.1&5.2

---
+++ Exercise5_1_Natchanop_Y.py
@@ -0,0 +1,8 @@
+print("Exercise 5.1")
+a=8
+b=7
+print("a = 8","a = 7")
+print("a+b =",a+b)
+print("a-b =",a-b)
+print("a*b =",a*b)
+print("a/b =",a/b)
---
+++ Exercise5_2_Natchanop_Y.py
@@ -0,0 +1,4 @@
+print("Speed Calcuator")
+s=float(input("Please input distance(Km) = "))
+t=float((input("Please input time (h) = ")))
+print("speed =",s/t,"km/h")
