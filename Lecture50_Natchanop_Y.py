9f1b1d636ce85b1d535763f4c22f4c0833b6fac5
DarkcontrolCC <87600728+DarkcontrolCC@users.noreply.github.com>
2021-07-29 17:37:29 +0700

    Lecture50

---
+++ Lecture50_Natchanop_Y.py
@@ -0,0 +1,8 @@
+def addnumber(x,y):
+    print("ผลบวก =",x+y)
+    print("ผลลบ =",x-y)
+    print("ผลคูณ =",x*y)
+    print("ผลหาร =",x/y)
+x=int(input("x : "))
+y=int(input("y : "))
+addnumber(x, y)
